7c31f20297d771dea16c9d8155b5943bc7be12b9
Kerim <kutukmu@gmail.com>
2020-10-27 21:40:51 -0500

    asd

--- src/redux/store.js
+++ src/redux/store.js
@@ -14,7 +14,7 @@ const reducers = combineReducers({
 })
 
 const store = createStore(reducers, initialState,
-    compose(applyMiddleware(...middleware),  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+    compose(applyMiddleware(...middleware)))
 
 
 
